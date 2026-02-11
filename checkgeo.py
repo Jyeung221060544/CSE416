f9979568aff636eb7ffbc379fe9d6c291cc5b5a2
Jason <jason1213yeung@gmail.com>
2026-02-11 16:07:34 -0500

    Update checkgeo.py

--- checkgeo.py
+++ checkgeo.py
@@ -18,15 +18,15 @@ print(or_csv["GEOID"].nunique())
 
 al_gdf = gpd.read_file("AL-precincts-with-results.geojson")
 
-print(or_gdf.columns)
-print(len(or_gdf))
-print(or_gdf.head())
+print(al_gdf.columns)
+print(len(al_gdf))
+print(al_gdf.head())
 
-print(or_gdf.geom_type.unique())
+print(al_gdf.geom_type.unique())
 
-print(or_gdf.crs)
+print(al_gdf.crs)
 
 al_csv = pd.read_csv("AL-precincts-with-results.csv")
 
-print(len(or_csv))
-print(or_csv["GEOID"].nunique())
+print(len(al_csv))
+print(al_csv["GEOID"].nunique())
