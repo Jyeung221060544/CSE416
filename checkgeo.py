30ef851056dc9d2486079848e71672602a1d1a8e
Jason <jason1213yeung@gmail.com>
2026-02-11 15:57:06 -0500

    files

---
+++ checkgeo.py
@@ -0,0 +1,32 @@
+import geopandas as gpd
+import pandas as pd
+
+or_gdf = gpd.read_file("OR-precincts-with-results.geojson")
+
+print(or_gdf.columns)
+print(len(or_gdf))
+print(or_gdf.head())
+
+print(or_gdf.geom_type.unique())
+
+print(or_gdf.crs)
+
+or_csv = pd.read_csv("OR-precincts-with-results.csv")
+
+print(len(or_csv))
+print(or_csv["GEOID"].nunique())
+
+al_gdf = gpd.read_file("AL-precincts-with-results.geojson")
+
+print(or_gdf.columns)
+print(len(or_gdf))
+print(or_gdf.head())
+
+print(or_gdf.geom_type.unique())
+
+print(or_gdf.crs)
+
+al_csv = pd.read_csv("AL-precincts-with-results.csv")
+
+print(len(or_csv))
+print(or_csv["GEOID"].nunique())
