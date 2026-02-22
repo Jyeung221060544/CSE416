d8ab4b6bb67f1707d78e47e4c6080971eb7b8ddb
ssumon1 <sumon.ahasan@stonybrook.edu>
2026-02-22 16:59:27 -0500

    initialized Spring Boot server and added MongoDB schemas

---
+++ backend/src/test/java/edu/stonybrook/cse416/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.stonybrook.cse416.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
